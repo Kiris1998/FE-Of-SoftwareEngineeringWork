dcaf132aae750ad7b891d4539a7bbd926837f930
kiris <kiriszhangjun_i@didiglobal.com>
2019-04-15 10:52:10 +0800

    init

--- src/pages/Welcome.js
+++ src/pages/Welcome.js
@@ -1,11 +1,4 @@
+/* eslint-disable jsx-a11y/label-has-associated-control */
 import React from 'react';
 
-export default () => (
-  <p style={{ textAlign: 'center' }}>
-    想要添加更多页面？请参考{' '}
-    <a href="https://umijs.org/guide/block.html" target="_blank" rel="noopener noreferrer">
-      umi 区块
-    </a>
-    。
-  </p>
-);
+export default () => <p style={{ textAlign: 'center' }}>asd</p>;
